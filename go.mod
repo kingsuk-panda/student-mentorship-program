30ce476ac8b83d8755eb267ee29298013dc78aed
kingsuk-panda <kingsuk.panda.2004@gmail.com>
2024-05-15 07:59:05 +0530

    Right now, I made a basic version of the main.go; will update as I go. user.go constructing rn, will update as soon as completed.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kingsuk-panda/student-mentorship-program
+
+go 1.21.9
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+
+	//http request handle karne ke liye
+
+	"./user"                      //user.go file ko import karne ke liye
+	"github.com/labstack/echo/v4" //Echo web framework ke liye
+)
+
+// In-memory user storage (database se replace kardena baadmein)
+var users []user.User
+
+func main() {
+	//Echo instance taiyyar karna hai
+	e := echo.New()
+
+	//user registration ke lilye route
+	e.POST("/register", user.RegisterHandler)
+
+	//server start karte hain
+	e.Logger.Fatal(e.Start(":8080")) // baad mein desired port number se replace karna hai
+}
+
+//abhi ke liye itna rehne dete hain.. baad ka dekhta hoon; pehle thoda user.go ka maamla dekhleta hoon
